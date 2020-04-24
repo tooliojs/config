dad34d8560b21a68d1add4334b912648b21dd91c
arakilian0 <arakilian0@gmail.com>
2020-04-23 20:24:35 -0700

    CREATED config 'relaxed' mode

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,12 @@
-# Changelog
+# Changelog 
 
-***BETA***
+*Note: currently in beta.*
 
-## v0.1.0
+## v0.2.1 - Relaxed Mode was born
+- First implementation of @toolio/config `relaxed` mode
+- Light testing was conducted, but needs more battle proofing
+- Currently undocumented
+
+## v0.1.0 - Initial Setup
 - Initial project setup
 - git, npm, test scripts, and doc files
--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 module.exports = function() {
-    console.log('running config')
+    const fs = require('fs')
+    const build = require('./lib/build')
+    const handle = require('./lib/handle')
+    const merge = require('./lib/_util/merge')
+    const __join = require('path').join
+    const __dirname = require('path').resolve()
+    const __homedir = require('os').homedir()
+    const settings = require('./lib/_util/settings')()
 
-    return undefined
+    let resolved
+    let non_resolved = []
+    let non_resolved_config = {}
+
+    settings.ext.forEach(ext => {
+        let check = settings.root
+        let file = `${settings.filename}.${ext}`
+        let home_path = __join(__homedir, check, file)
+        let local_path = __join(__dirname, check, file)
+
+        if(fs.existsSync(home_path)) non_resolved.push(build(home_path))
+        if(fs.existsSync(local_path)) non_resolved.push(build(local_path))
+    })
+
+    if(!non_resolved.length) return undefined
+
+    if(non_resolved.length === 1) non_resolved_config = non_resolved[0]
+    else non_resolved_config = merge(non_resolved[0], non_resolved[1])
+
+    if(!Object.keys(non_resolved_config).length) { console.log('error: no configs detected'); process.exit() }
+    else resolved = handle(non_resolved_config)
+
+    console.log(resolved)
+    return 0
 }()
