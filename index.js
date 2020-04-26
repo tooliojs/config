587b2c9a4e7f4366c26ad1711a1339a97cd6c4eb
arakilian0 <arakilian0@gmail.com>
2020-04-25 18:40:48 -0700

    UPDATED strict mode - plugin usage

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ module.exports = function() {
     const __dirname = require('path').resolve()
     const __homedir = require('os').homedir()
     const settings = require('./lib/_util/settings')()
+    const module_exists = require('./lib/_util/module_exists')
+    const package_exists = require('./lib/_util/package_exists')
 
     let resolved
     let __paths = []
@@ -48,6 +50,21 @@ module.exports = function() {
     let builds = {}
     __paths.forEach(_p => { merge(builds, build(_p)) })
     resolved = builds
+
+    if(builds && builds.config && builds.config.mode && builds.config.mode === 'strict') {
+        for(key in builds) {
+            if(key === 'config') continue
+            let val_object
+            if(package_exists(builds.config.schemas[key])) val_object = require(__join(__dirname, 'node_modules', builds.config.schemas[key]))
+            else if(module_exists(builds.config.schemas[key])) val_object = module_exists(builds.config.schemas[key]).plugin
+
+            let pre_val_obj = {}
+            pre_val_obj[key] = builds[key]
+
+            let { error } = val_object.validate(pre_val_obj)
+            if(error) { console.log(error); process.exit() }
+        }
+    }
 
     if(builds && builds.config && builds.config.exclude && builds.config.exclude.length && typeof builds.config.exclude !== 'string') {
         builds.config.exclude.forEach(co_key => {
